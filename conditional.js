e5ebcb01e266253ff6653b02f4d9e6c06ef9540e
rizukious <rizukiwahyudi"gmail.com>
2019-07-14 14:07:50 +0700

    tambah komment

--- conditional.js
+++ conditional.js
@@ -4,11 +4,5 @@ if (1 != 1){
 	console.log('tapi 1 lebih dari 0');//kondisi kedua
 } else{
 	console.log('ambil semua bro')//kondisi terakhir
-}
-var nilai;
-console.log('masukkan nilai anda:'+ nilai)
-if (nilai > 70){
-	console.log('nilai anda c');
-} else if (nilai = 70){
-	console.log('nilai anda b');
+	console.log('a');
 }
