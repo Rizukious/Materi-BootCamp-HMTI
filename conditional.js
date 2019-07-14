5d49b1fd65339e69aee2434acc3521c0baef837a
rizukious <rizukiwahyudi"gmail.com>
2019-07-14 13:54:18 +0700

    initial project

---
+++ conditional.js
@@ -0,0 +1,14 @@
+if (1 != 1){
+	console.log('1 tidak sama dengan 1');//kondisi pertama
+} else if (1 < 1){
+	console.log('tapi 1 lebih dari 0');//kondisi kedua
+} else{
+	console.log('ambil semua bro')//kondisi terakhir
+}
+var nilai;
+console.log('masukkan nilai anda:'+ nilai)
+if (nilai > 70){
+	console.log('nilai anda c');
+} else if (nilai = 70){
+	console.log('nilai anda b');
+}
